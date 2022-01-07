ea10cab8efde1ef3b47c13d863fb0ac4164d81fc
Cyrille MdC <cyrille.mdc@gmail.com>
2022-01-07 09:23:01 +0100

    progress

--- 2021/day23/day23.py
+++ 2021/day23/day23.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 input_file = 'test_input'
-#input_file = 'input'
+input_file = 'input'
 
 def matPrint(mat):
     h,w = mat.shape
@@ -12,21 +12,66 @@ def matPrint(mat):
     print()
 
 cave = []
+# '#' walls
+# '.' space to move through at cost
+# '*' or letter are places one can rest
+
+# snails only move into their cave if it's either empty or has one of their types there
+# each snail gets two moves maximum, of any distance, one out of cave, then in a cave
+
+class Snail:
+    def __init__(self, stype, pos):
+        self.type = stype
+        self.pos = pos
+
+    def __str__(self):
+        return(self.type + "@" + str(self.pos))
+
+    def getPossibleMoves(self, cave):
+        # find all accessible locations of '_' in cave
+        pass
+
+snails = []
+ezones = dict()
 with open(input_file, 'r') as fh:
-    w = None
-    h = 0
+    rownum = 0
     for line in fh:
         line = line.strip('\n')
-        if w == None:
-            w = len(line)
-        while len(line) < w:
-            line += ' '
-        cave.append(list(line))
-        h += 1
-cave = np.array(cave).reshape((h,w))
+        row = []
+        colnum = -1
+        ezstr = list('DCBA')
 
-def getPossibleMoves(
+        for c in list(line):
+            colnum += 1
+            if c == '#':
+                row.append('#')
+                continue
+            if c == '*':
+                row.append('_')
+                continue
+            if c == '.': 
+                row.append('.')
+                continue
+            
+            # rest are snails
+            # spots are occupied
+            row.append('o')
+            pos = [rownum, colnum]
+            snails.append(Snail(c, pos))
+
+            # need to create exclusion zones based on snail types
+            if rownum > 1:
+                ezones[str(pos)] = ezstr.pop()
+
+        rownum += 1
+
+        cave.append(row)
+cave = np.array(cave)
+
 matPrint(cave)
+[print(s) for s in snails]
+print(ezones)
+
 
 # movement energy requirements
 mnrg = {'A': 1,
@@ -38,7 +83,7 @@ mnrg = {'A': 1,
 # - can't stop at room exits
 # - only stop once outside room
 # - only go to room as final destination
-# - really, each snail only has two moves max (can move directly from room to room through)
+# - each snail has two moves max (can move directly from room to room through)
 
 ##### PART 1 #####
 print("Part 1")
