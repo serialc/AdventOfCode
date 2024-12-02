8694fcc580c8b0794ebe7afef4d61d14d1072da6
Cyrille Médard de Chardon <cyrille@frakturmedia.net>
2024-12-02 09:14:19 +0100

    day2

--- 2024/README.md
+++ 2024/README.md
@@ -8,9 +8,11 @@ Day:
 Day:
 
 1. Probably easier in R
+2. Also easier in R, pull out numpy
 
 ### Tags
 
 Day:
 
 1. dictionary, frequency-distribution
+2. boolean, difference
---
+++ 2024/day02/day02.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+input_file = 'input0'
+input_file = 'input'
+
+safecount = 0
+with open(input_file, 'r') as fh:
+    for l in fh:
+        l = l.strip('\n')
+
+        if l == '':
+            continue
+
+        lvls = np.array(list(map(int, l.split())))
+        diffs = np.diff(lvls)
+
+        if (np.all(diffs > 0) or np.all(diffs < 0)) and np.all(np.abs(diffs) <= 3):
+            safecount += 1
+
+print("#### Part 1 ####")
+print("Answer is:", safecount)
+
+#### PART 2 ####
+print("============ Part 2 start ================")
+
+safecount = 0
+with open(input_file, 'r') as fh:
+    for l in fh:
+        l = l.strip('\n')
+
+        if l == '':
+            continue
+
+        lvls = np.array(list(map(int, l.split())))
+        diffs = np.diff(lvls)
+
+        if (np.all(diffs > 0) or np.all(diffs < 0)) and np.all(np.abs(diffs) <= 3):
+            safecount += 1
+            continue
+
+        # can we make the report safe?
+        # try removing each one and see
+        for i in range(len(lvls)):
+            sublvls = np.delete(lvls, i)
+            diffs = np.diff(sublvls)
+            if (np.all(diffs > 0) or np.all(diffs < 0)) and np.all(np.abs(diffs) <= 3):
+                safecount += 1
+                break
+
+print("#### Part 2 ####")
+print("Answer is:", safecount)
