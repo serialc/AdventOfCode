03cbf28a7fe5b6142ae46ec56dbb17253daf1d45
Cyrille Médard de Chardon <cyrille.mdc@gmail.com>
2025-12-16 10:59:32 +0100

    Used bitwise operation rather than changing characters and cleaned up

--- 2025/day10/day10p1.py
+++ 2025/day10/day10p1.py
@@ -3,8 +3,9 @@
 import sys
 import numpy as np
 import re
-import time
-import functools  # for memoization
+
+# import time
+# import functools  # for memoization
 
 # import math
 # from PIL import Image
@@ -14,18 +15,6 @@ input_file = "input"
 if len(sys.argv) == 2:
     input_file += sys.argv[1]
 print("\nProcessing input file:", input_file)
-
-lights = []
-bcombs = []
-with open(input_file, "r") as fh:
-    step = 0
-    for line in fh:
-        line = line.strip("\n")
-
-        lights.append(re.findall(r"\[(.+)\]", line)[0])
-        bcl = re.findall(r"\] (.+) \{", line)
-        bcl2 = bcl[0].replace("(", "").replace(")", "").split(" ")
-        bcombs.append([[int(y) for y in x.split(",")] for x in bcl2])
 
 
 def rsearch(goal, state, bset, pressedset):
@@ -43,21 +32,17 @@ def rsearch(goal, state, bset, pressedset):
     # line below is key! only search a subset as we go deeper into recursion!
     for bs in bset[len(pressedset) :]:
 
-        # can only press each button a maximum of one time
+        # !! can only press each button a maximum of one time
         if bs in pressedset:
             continue
 
         tstate = state.copy()
         # combos have 1 or more buttons
         for b in bs:
-            if tstate[b] == ".":
-                tstate[b] = "#"
-            else:
-                tstate[b] = "."
-
-        # print(" " * len(pressedset), tstate)
+            tstate[b] ^= 1
 
         score = rsearch(goal, tstate, bset, pressedset + [bs])
+
         if score is not False and score < lowest:
             lowest = score
 
@@ -65,11 +50,25 @@ def rsearch(goal, state, bset, pressedset):
 
 
 results = []
-for i in range(len(lights)):
-    print("Searching:", lights[i], "Sum: ", end="")
-    res = rsearch(list(lights[i]), list("." * len(lights[i])), bcombs[i], [])
-    results.append(res)
-    print(res, "Current sum:", sum(results))
+with open(input_file, "r") as fh:
+    step = 0
+    for line in fh:
+        line = line.strip("\n")
+
+        char_lights = re.findall(r"\[(.+)\]", line)[0]
+        digit_lights = char_lights.replace(".", "0").replace("#", "1")
+        lights = [int(x) for x in list(digit_lights)]
+        # print("Goal", lights, end="")
+
+        bcl = re.findall(r"\] (.+) \{", line)
+        bcl2 = bcl[0].replace("(", "").replace(")", "").split(" ")
+        bcombs = [[int(y) for y in x.split(",")] for x in bcl2]
+        # print("Button connections", bcombs)
+
+        # send goal, off state, btn connections, btn presses
+        res = rsearch(lights, [0] * len(lights), bcombs, [])
+        results.append(res)
+        # print("Button presses", res)
 
 # print(results)
 print("#### Part 1 ####")
